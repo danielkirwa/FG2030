2977cd660c85c1cb15edd301f23bd219b75941af
danielkirwa <kirwadan03@gmail.com>
2024-02-28 00:56:06 +0300

    Transaction selector fixed and more design logic

--- app/src/main/java/com/example/myapplication/Dashboard.java
+++ app/src/main/java/com/example/myapplication/Dashboard.java
@@ -6,14 +6,20 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Dashboard extends AppCompatActivity {
 private DrawerLayout drawerLayout;
+Button openTransaction;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashbaord);
+
+        openTransaction = findViewById(R.id.btn_open_transaction);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -22,6 +28,15 @@ private DrawerLayout drawerLayout;
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle( this, drawerLayout, toolbar,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+        // open transactions
+        openTransaction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentOpenTransaction = new Intent(Dashboard.this,Transaction.class);
+                startActivity(intentOpenTransaction);
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/example/myapplication/Transaction.java
@@ -0,0 +1,127 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
+
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Calendar;
+
+public class Transaction extends AppCompatActivity {
+EditText pickDate;
+CardView inTransaction, outTransaction;
+Button saveTransaction;
+int transactionTypeState = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaction);
+        pickDate = findViewById(R.id.txt_transaction_date);
+        inTransaction = findViewById(R.id.in_transaction);
+        outTransaction = findViewById(R.id.out_transaction);
+        saveTransaction = findViewById(R.id.btn_save_transaction);
+
+
+
+
+        inTransaction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                transactionTypeState = 1;
+               saveTransaction.setBackgroundTintList(ContextCompat.getColorStateList(Transaction.this, R.color.green));
+            }
+        });
+        outTransaction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                transactionTypeState = 2;
+                saveTransaction.setBackgroundTintList(ContextCompat.getColorStateList(Transaction.this, R.color.crimson));
+            }
+        });
+
+        // do transaction
+        saveTransaction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              // check state of the transaction type
+                if (transactionTypeState == 0){
+                    showMessage("Transaction Not Selected","Select transaction");
+                } else if (transactionTypeState == 2) {
+                    showMessage("Transaction Out","Transaction type outgoing amount");
+                } else if (transactionTypeState == 1) {
+                    showMessage("Transaction In","Transaction type incoming amount");
+                }else {
+                    showMessage("Unknown Error","Transaction type error try selecting again");
+                }
+            }
+        });
+
+        pickDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDateTimePicker();
+            }
+        });
+
+    }// end of onCreate
+    // date picker select date time of transaction
+    private void showDateTimePicker() {
+        // Get current date and time
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+        int hourOfDay = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+
+        // Create date picker dialog
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+                (view, year1, monthOfYear, dayOfMonth1) -> {
+                    // Set selected date to EditText
+                    String selectedDate = dayOfMonth1 + "/" + (monthOfYear + 1) + "/" + year1;
+
+                    // Create time picker dialog
+                    TimePickerDialog timePickerDialog = new TimePickerDialog(
+                            this,
+                            (view1, hourOfDay1, minute1) -> {
+                                // Set selected time to EditText
+                                String selectedTime = " " + hourOfDay1 + ":" + minute1;
+                                pickDate.setText(selectedDate + selectedTime);
+                            },
+                            hourOfDay,
+                            minute,
+                            true // 24-hour time format
+                    );
+
+                    // Show time picker dialog after date is selected
+                    timePickerDialog.show();
+                },
+                year,
+                month,
+                dayOfMonth
+        );
+
+        // Show date picker dialog
+        datePickerDialog.show();
+    }
+    // create pop up message template
+    @SuppressLint("UseCompatLoadingForDrawables")
+    public  void showMessage(String popUpTitle, String popupMessage){
+        androidx.appcompat.app.AlertDialog.Builder builder = new androidx.appcompat.app.AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(popUpTitle);
+        builder.setMessage(popupMessage);
+        builder.setIcon(getResources().getDrawable(R.drawable.money));
+        builder.setPositiveButton("OK",null);
+        builder.show();
+    }
+
+}
