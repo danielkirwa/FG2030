e81b3f556c134829cfcf7c8edad4952bfbb6ef08
danielkirwa <kirwadan03@gmail.com>
2024-03-03 12:02:14 +0300

    Connected to firebase now working

--- app/src/main/java/com/example/myapplication/CreatePurpose.java
+++ app/src/main/java/com/example/myapplication/CreatePurpose.java
@@ -3,10 +3,14 @@ package com.example.myapplication;
 import androidx.annotation.LayoutRes;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
@@ -14,12 +18,14 @@ import java.util.List;
 
 public class CreatePurpose extends AppCompatActivity {
 Spinner transaction_Category;
+Button savePurpose;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_purpose);
 
         transaction_Category = findViewById(R.id.transaction_item_category);
+        savePurpose = findViewById(R.id.btn_save_transaction_item);
 
 
         // add array item to category items
@@ -31,5 +37,20 @@ Spinner transaction_Category;
         ArrayAdapter<String> adapter = new ArrayAdapter<>(CreatePurpose.this, android.R.layout.simple_spinner_item, itemList);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         transaction_Category.setAdapter(adapter);
+
+
+
+        // insert purpose to db
+        // Write a message to the database
+        savePurpose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseDatabase database =  FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("Purpose");
+
+                myRef.setValue("Hello, World!");
+            }
+        });
+
     }
 }
